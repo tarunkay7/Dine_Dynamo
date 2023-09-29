ffafef5d2468f0fda22d5e7871dc16a1493ccdb8
tarun <tarunmenon383@gmail.com>
2023-09-29 15:26:59 +0530

    added new files

--- dinedynamo/src/main/java/com/project/dinedynamo/APIController/ItemsControllerAPI.java
+++ dinedynamo/src/main/java/com/project/dinedynamo/APIController/ItemsControllerAPI.java
@@ -3,12 +3,14 @@ package com.project.dinedynamo.APIController;
 import com.project.dinedynamo.Entities.Items;
 import com.project.dinedynamo.Service.ItemsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/items")
+@Controller
+@RequestMapping("/items")
 public class ItemsControllerAPI {
 
     @Autowired
@@ -19,14 +21,27 @@ public class ItemsControllerAPI {
         return itemsservice.addItems(items);
     }
 
-    @GetMapping("/getByCategory")
-    public List<Items> getItemsByCategory(@RequestParam String category) {
-        return itemsservice.getItemsByCategory(category);
+    @GetMapping("/ItemsByCategory")
+    public String getItemsByCategory(Model model, @RequestParam("") String category) {
+        System.out.println("Entering getItemsByCategory method");
+
+        List<Items> items = itemsservice.getItemsByCategory("Lunch");
+        System.out.println("Number of items retrieved: " + items.size());
+
+        model.addAttribute("catitems", items);
+        System.out.println("Added 'catitems' attribute to the model");
+
+        System.out.println("Exiting getItemsByCategory method");
+
+        return "catitems";
     }
 
-    @GetMapping("/getAll")
-    public List<Items> getAll() {
-        return itemsservice.getAllItems();
+
+    @GetMapping("/menu")
+    public String getAll(Model model){
+        List<Items> items = itemsservice.getAllItems();
+        model.addAttribute("items", items);
+        return "menu";
     }
 
     @GetMapping("/getByPrice")

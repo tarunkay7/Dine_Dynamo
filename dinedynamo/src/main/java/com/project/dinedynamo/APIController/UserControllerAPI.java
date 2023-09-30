faa8ca831415a28aa908ae142ff1c1f3f740fea1
tarun <tarunmenon383@gmail.com>
2023-09-30 23:58:59 +0530

    Added functionality for email and phone number

--- dinedynamo/src/main/java/com/project/dinedynamo/APIController/UserControllerAPI.java
+++ dinedynamo/src/main/java/com/project/dinedynamo/APIController/UserControllerAPI.java
@@ -3,12 +3,14 @@ package com.project.dinedynamo.APIController;
 import com.project.dinedynamo.Entities.User;
 import com.project.dinedynamo.Service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/users")
+@Controller
+@RequestMapping("/users")
 public class UserControllerAPI {
 
     @Autowired
@@ -30,6 +32,7 @@ public class UserControllerAPI {
     {
         return userservice.getAllUsers();
     }
+
     
 }
 
